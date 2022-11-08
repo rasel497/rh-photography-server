81389466cb338f4a133b69a19f16f7d9381a5640
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-09 03:28:16 +0600

    server connted to MongoDb database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 require('colors');
 const app = express();
@@ -8,6 +9,22 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// connect database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function runDbConnect() {
+    try {
+        await client.connect();
+        console.log('Database connected'.yellow.italic);
+    }
+    catch (error) {
+        console.log(error.name.bgRed, error.message.bold, error.stack);
+    }
+}
+runDbConnect();
 
 
 // Basic initial setup
