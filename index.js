e82f59cad152077d85e8a607827d43534beacb4a
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-09 21:34:02 +0600

    six service data loaded from mongoDb and show client site

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function runDbConnect() {
+async function run() {
+
     try {
-        await client.connect();
-        console.log('Database connected'.yellow.italic);
+        const serviceCollection = client.db('rhPhotography').collection('services');
+
+        // get all data using Find Multiple option
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
     }
-    catch (error) {
-        console.log(error.name.bgRed, error.message.bold, error.stack);
+
+    finally {
+
     }
 }
-runDbConnect();
+run().catch(err => console.error(err));
 
 
 // Basic initial setup
